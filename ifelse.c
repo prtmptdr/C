8d39ab2ade42157726608b40651d17a0294a9245
prtmptdr <168543405+prtmptdr@users.noreply.github.com>
2024-06-08 09:36:11 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+     {
+printf("india mobile num\n");
+}
+long long num;
+
+scanf("%lld",&num);
+if (num>=6000000000 && num<=9999999999){
+
+printf("the num is legal");
+
+}
+else{
+    printf("the num is illegal");
+}
+
+}
