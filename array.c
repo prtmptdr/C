99b4f3140bbcb04ba8aafb3b681f292f8e25471d
prtmptdr <168543405+prtmptdr@users.noreply.github.com>
2024-06-08 09:37:04 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,166 @@
+#include<stdio.h>
+void main(){
+// int marks[6];
+// printf("pls ente 6 array element of integer type");
+// for(int i=0; i<6; i++)
+// scanf("%d",& marks[i]);
+// printf("my array is print below\n");
+// for(int i=0; i<6; i++)
+// printf("%d |",marks[i]);
+
+// }
+// char name[4];
+// printf("pls enter 4 array element of char type\n");
+// for(int i=0; i<4; i++)
+// scanf(" %c",&name[i]);
+// printf("my array is print below\n");
+// for(int i=0; i<4; i++)
+// printf(" %c  |",name[i]);
+// }
+// int marks[2][4];
+// printf("pls enter 8 array element of int type\n");
+// for(int i=0; i<2; i++)
+// for(int j=0; j<4; j++)
+// scanf("%d",&marks[i][j]);
+// for(int i=0; i<2; i++){
+// for(int j=0; j<4; j++){
+// printf("%d |",marks[i][j]);
+// }
+// printf("\n");
+// }
+// }
+// int marks[2][2][2];
+// printf("pls enter 8 array element of int type\n");
+// for(int i=0; i<2; i++)
+// for(int j=0; j<2; j++)
+// for(int k=0; k<2; k++)
+// scanf("%d",&marks[i][j][k]);
+// for(int i=0; i<2; i++){
+// for(int j=0; j<2; j++){
+// for(int k=0; k<2; k++){
+// printf("%d |",marks[i][j][k]);
+// }
+// printf("\n");
+//  }
+// printf("\n\n");
+// }
+// }
+// printf("pls enter elemension any 10 nos\n");
+// int array[10];
+// for(int i=0; i<10; i++)
+// scanf("%d",&array[i]);
+// for(int i=0; i<10; i++)
+// printf("%d |",array[i]);
+// }
+  
+
+
+
+// int array[6]={20, -5, 2, -9 ,6 ,78};
+// for(int i=0; i<6; i++)
+// for(int j=0; j<5; j++){
+// if(array[j]>array[j+1]){
+// int temp=array[j+1];
+// array[j+1]=array[j];
+// array[j]=temp;
+
+
+// }
+// }
+// for(int i=0; i<6; i++)
+// printf("%d |",array[i]);
+// }
+
+
+//  int array[6]={20, -5, 2, -9 ,6 ,78};
+// int flag =0;
+// for(int i=0; i<6; i++)
+// if(array[i]==5){
+// printf("item found  on index %d",i);
+// flag = 1;
+// }
+// if(flag==0)
+// printf("item not found");
+// }
+
+
+// int array[6]={20, -5, 2, -9 ,6 ,78};
+// for(int i=0; i<6; i++)
+// if(array[0] < array[i])
+// array[0] = array[i];
+// printf("the greatest value %d",array[0]);
+// }
+
+
+
+
+// #                                 TWO D ARRAY
+
+
+// int arr [3][3],trans[3][3];
+// printf("pls enter values");
+// for(int i =0; i<3 ;i++)
+// for(int j=0;j<3; j++) 
+// scanf("%d" , &arr[i][j]);
+// printf("the array is..\n");
+// for(int i =0; i<3 ;i++){
+//   for(int j=0;j<3; j++) {
+//   printf("%d |" ,arr[i][j]);
+//   }
+//   printf("\n");
+// }
+
+
+
+
+
+
+
+
+
+// printf("pls enter number of rows/coloum");
+// int arr[3][3];
+// for(int i=0; i<3; i++){
+// for(int j = 0 ; j <3; j++){
+// scanf("%d , arr[i][j]");
+
+// }
+// }
+
+// for(int i=0; i<3; i++){
+// for(int j = 0 ; j <3; j++){
+//   int temp = arr[i][j];
+//   arr[i][j] = arr [j][i];
+//   arr[j][i] = temp;
+// }
+// } 
+// for(int i=0; i<3; i++){
+// for(int j = 0 ; j<3; j++){
+//   printf("%d , arr[j][i]");
+// }
+// printf("\n");
+//}
+for ( int i=10; i>=0; i--) {
+for ( int j=0; j<=i; j++) {
+printf("*");
+}
+printf("\n");
+}
+
+for ( int i=0; i<=10; i++) {
+for ( int j=0; j<=i; j++) {
+printf("*");
+}
+printf("\n");
+}
+
+
+
+
+
+
+
+
+}
+
+
