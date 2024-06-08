fcbc41dd544a0fc717614d711a8440af7f02d617
prtmptdr <168543405+prtmptdr@users.noreply.github.com>
2024-06-08 09:36:01 +0530

    Add files via upload

---
+++ bitwise.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main(){
+
+
+// printf("plese enter two num\n");
+// int num1 ,num2;
+// scanf("%d%d",&num1,&num2);
+// if (num1>num2){
+
+// printf("the greater num is %d",num1);
+
+// }
+// else if (num1==num2){
+// printf("the number are equal");
+
+// }
+// else{
+//     printf("the greater num is %d",num2);
+// }
+
+// }
+
+
+
+int x=20;
+int y=30;
+int z=x^y;
+printf("%d",z);
+
+}
