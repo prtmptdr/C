c8c59f848092a5f47254655ef2f49d116d500da8
prtmptdr <168543405+prtmptdr@users.noreply.github.com>
2024-06-08 09:35:46 +0530

    Add files via upload

---
+++ union.c
@@ -0,0 +1,39 @@
+// #include<stdio.h>
+// #include<string.h>
+// union btech {
+// char name[50];
+// long long mobile;
+// char college[50];
+// char branch[50];
+// };
+// void main(){
+// union btech pritam;
+// printf("enter name of pritam\n");
+// char nm[50];
+// scanf("%[^\n]",&nm);
+// getchar();
+// printf("pls enter mo.nos\n");
+// scanf("%lld",&pritam.mobile);
+// getchar();
+// printf("pls enter college name\n");
+// char col[50];
+// scanf("%[^\n]",&col);
+// getchar();
+// printf("pls enter branch name\n");
+// char br[50];
+// scanf("%[^\n]",&br);
+// printf("%s\n%lld\n%s\n%s\n",strcpy(pritam.name,nm),pritam.mobile,strcpy(pritam.college,col),strcpy(pritam.branch,br));
+// }
+
+
+
+#include<stdio.h>
+void add(){
+int a;
+int b;
+printf("pls enter a nos\n");
+
+
+
+}
+ 
